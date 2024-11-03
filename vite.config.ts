240d952cbcc1ab24e32091b5300cd4e645dd82fa
Wojciech Marek <marek.wojciech@outlook.com>
2024-11-03 19:14:34 +0100

    feat: add lucide icons and add footer menu

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      includeAssets: ['favicon.ico', 'vite.svg'],
+      includeAssets: ["favicon.ico", "vite.svg"],
       manifest: {
-        name: 'Stocky',
-        short_name: 'Stocky',
-        description: 'The personal investments portfolio manager',
-        theme_color: '#141a21',
+        name: "Stocky",
+        short_name: "Stocky",
+        description: "The personal investments portfolio manager",
+        theme_color: "#141a21",
         icons: [
           {
-            src: 'stocky.png',
-            sizes: '256x256',
-            type: 'image/png',
+            src: "stocky.png",
+            sizes: "256x256",
+            type: "image/png",
           },
         ],
       },
     }),
   ],
-})
+});
