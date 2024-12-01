8430aa9b0e7f0e57e2eef8865c9fb58f4f215023
Wojciech Marek <marek.wojciech@outlook.com>
2024-12-01 12:58:11 +0100

    feat: update the back button style

--- src/components/molecules/common-back-button/CommonBackButton.tsx
+++ src/components/molecules/common-back-button/CommonBackButton.tsx
@@ -4,9 +4,9 @@ import { ChevronLeft } from "lucide-react";
 
 export const CommonBackButton = () => {
   return (
-    <div className="flex flex-row mx-3 mt-6 items-center justify-between">
+    <div className="flex flex-row mt-6 items-center justify-between">
       <Link to={"/overview"}>
-        <Button className="bg-transparent p-3 flex flex-row items-center">
+        <Button className="bg-transparent p-2 flex flex-row items-center">
           <div className="h-6 w-6">
             <ChevronLeft />
           </div>
